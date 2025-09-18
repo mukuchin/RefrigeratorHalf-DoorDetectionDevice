c00465b40bd645c09c9256767afc11bafce3496d
mukuchin <haruto.mukuno@gmail.com>
2025-09-18 13:34:48 +0900

    ホールセンサのデータシートをREADMEに追加した。

--- main.ino
+++ main.ino
@@ -1,12 +1,21 @@
-int LED_PIN = 3;
+int OPEN_LED_PIN = 6;
+int CLOSE_LED_PIN = 2;
+int HALL_SENSOR_PIN1 = 7;
+int HALL_SENSOR_PIN2 = 8;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
+  pinMode(HALL_SENSOR_PIN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-  delay(200);
+  if (digitalRead(HALL_SENSOR_PIN) == HIGH)
+  {
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    delay(200);
+  }
+  else
+  {
+    digitalWrite(LED_PIN, LOW);
+  }
 }
